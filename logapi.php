2caf56bd237e1397b2a98c2075ab5f6278b9dadc
Graham R. Cobb <g+git-default@cobb.uk.net>
2016-10-11 23:00:31 +0100

    Auth fixes

--- logapi.php
+++ logapi.php
@@ -11,13 +11,35 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
 }
 include "php-crud-api/api.php";
 
+function auth_command($cmd,$db,$tab) {
+	 // Read, list - always allowed
+	 if ($cmd=='read' || $cmd=='list') return true;
+	 
+	 // Create allowed if username is provided
+	 if ($cmd=='create') return isset($_SERVER['REMOTE_USER']) || isset($_SERVER['PHP_AUTH_USER']);
+	 
+	 // Otherwise, check that username begins "full-" to permit access
+	 if (isset($_SERVER['PHP_AUTH_USER'])) { $remote_user = $_SERVER['PHP_AUTH_USER']; }
+	 elseif (isset($_SERVER['REMOTE_USER'])) { $remote_user = $_SERVER['REMOTE_USER']; }
+	 else $remote_user = null;
+	 return substr($remote_user, 0, 5) == "full-" ;
+}
+
 function tenancy_username($cmd,$db,$tab,$col) {
-	 if ($cmd=='read' || $cmd=='list' || $col<>'username') return null;
-	 return $_SERVER['PHP_AUTH_USER'];
+	 if ($col!='username') return null;
+	 
+	 // Update commands must set the username
+	 if ($cmd=='read' || $cmd=='list') return null;
+	 if (isset($_SERVER['PHP_AUTH_USER'])) {
+	    return $_SERVER['PHP_AUTH_USER'];
+	 } else {
+	    return $_SERVER['REMOTE_USER'];
+	 }
 }
 
 function auth_cols($cmd,$db,$tab,$col) {
-	 return $cmd=='read' || $cmd=='list' || ($col<>'id' && $col<>'log_time');
+	 // Updates cannot change id or log_time
+	 return $cmd=='read' || $cmd=='list' || ($col!='id' && $col!='log_time');
 }
 
 $api = new PHP_CRUD_API(array(
@@ -26,6 +48,7 @@ $api = new PHP_CRUD_API(array(
 		'username'=>'testuser',
 		'password'=>'test123test!',
 		'database'=>'testdb',
+		'table_authorizer'=>'auth_command',
 		'tenancy_function'=>'tenancy_username',
 		'column_authorizer'=>'auth_cols'
 ));
