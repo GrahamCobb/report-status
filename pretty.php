0d5dad21629448b0a97aed8b8ada712ba2c2c264
Graham R. Cobb <g+git-default@cobb.uk.net>
2016-10-17 11:56:13 +0100

    Replace use of iterators

--- pretty.php
+++ pretty.php
@@ -2,24 +2,6 @@
 echo "<table style='border: solid 1px black;'>\n";
 echo "<tr><th>Source</th><th>Target</th><th>Status</th><th>Log</th><th>Time</th></tr>\n";
 
-class TableRows extends RecursiveIteratorIterator {
-    function __construct($it) {
-        parent::__construct($it, self::LEAVES_ONLY);
-    }
-
-    function current() {
-        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-    }
-
-    function beginChildren() {
-        echo "<tr>";
-    }
-
-    function endChildren() {
-        echo "</tr>" . "\n";
-    }
-}
-
 $dbserver = "localhost";
 $dbuser = "statusreports";
 $dbpass = "password";
@@ -46,16 +28,27 @@ try {
 	    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	    if (count($rows) > 0) {
 	       $log_time = $rows[0]['log_time'];
-	       foreach(new TableRows(new RecursiveArrayIterator($rows)) as $k=>$v) {
-	            echo $v;
+	       foreach ($rows as $row) {
+	       	       echo "<tr>";
+		       echo "<td style='width:150px;border:1px solid black;'>".$row['source']."</td>";
+		       echo "<td style='width:150px;border:1px solid black;'>".$row['target']."</td>";
+		       echo "<td style='width:150px;border:1px solid black;'>".$row['status']."</td>";
+		       echo "<td style='width:150px;border:1px solid black;'>".$row['log_text']."</td>";
+		       echo "<td style='width:150px;border:1px solid black;'>".$row['log_time']."</td>";
+		       echo "</tr>";
 	       }
 
 	       /* And any subsequent log messages */
 	       $stmt = $conn->prepare("SELECT source,target,status,log_text,log_time FROM $dbtable WHERE source='$s' AND target='$t' AND status IS NULL AND log_time >= '$log_time' ORDER BY log_time DESC");
 	       $stmt->execute();
-	       $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	       foreach(new TableRows(new RecursiveArrayIterator($rows)) as $k=>$v) {
-	            echo $v;	    
+	       foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
+	       	       echo "<tr>";
+		       echo "<td style='width:150px;border:1px solid black;'>".$row['source']."</td>";
+		       echo "<td style='width:150px;border:1px solid black;'>".$row['target']."</td>";
+		       echo "<td style='width:150px;border:1px solid black;'>".$row['status']."</td>";
+		       echo "<td style='width:150px;border:1px solid black;'>".$row['log_text']."</td>";
+		       echo "<td style='width:150px;border:1px solid black;'>".$row['log_time']."</td>";
+		       echo "</tr>";
 	       }
 	   } 
     }
